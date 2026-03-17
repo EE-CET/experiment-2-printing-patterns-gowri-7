2a64bf431f4ce529208d7ee301ed63bb2e9c3596
gowri-7 <166044015+gowri-7@users.noreply.github.com>
2026-03-17 20:26:32 +0530

    Update PatternPrinting.java

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class PatternPrinting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter n");
         int n = sc.nextInt();
 
         for(int i = 0; i < n; i++) {
@@ -15,5 +16,4 @@ public class PatternPrinting {
         }
     }
 }
-    
 
